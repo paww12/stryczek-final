db0dd0b559593af94eb8287288d40d22bdf08d1d
aleks3650 <94302102+aleks3650@users.noreply.github.com>
2025-03-19 20:08:34 +0100

    initial setup

---
+++ src/app/(frontend)/page.tsx
@@ -0,0 +1,20 @@
+import { headers as getHeaders } from 'next/headers.js'
+import { getPayload } from 'payload'
+import React from 'react'
+
+import config from '@/payload.config'
+import './styles.css'
+
+export default async function HomePage() {
+  const headers = await getHeaders()
+  const payloadConfig = await config
+  const payload = await getPayload({ config: payloadConfig })
+  const { user } = await payload.auth({ headers })
+
+  return (
+    <div className="bg-slate-300">
+      <h1>hello stryczek</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  )
+}
